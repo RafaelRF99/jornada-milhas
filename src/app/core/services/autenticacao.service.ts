13d4090eae10439ba9467fef88b41ccc75d309e8
Rafael Ferreira <rafaelfferreira99@hotmail.com>
2023-11-28 16:23:51 -0300

    feat: service auth

---
+++ src/app/core/services/autenticacao.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { environment } from './../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AutenticacaoService {
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  autenticar(email: string, senha: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/auth/login`, { email, senha });
+  }
+}
